c2e5e2fbf1f4433f8c7beca144c4c5b5dfcdddb9
Joao Oliveira <jhcardosodeoliveira@gmail.com>
2024-09-16 15:22:30 +0100

    Add project files.

---
+++ Models/Heros.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeros.Models
+{
+    public class Heros
+    {
+        public string Name { get; set; }
+        public string HeroName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{HeroName} - {Name}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using SuperHeros.Models;
+
+Heros heroOne = new Heros();
+heroOne.Name = "Peter Parker";
+heroOne.HeroName = "Spider Man";
+
+Heros heroTwo = new Heros();
+heroTwo.Name = "Bruce Wayne";
+heroTwo.HeroName = "Batman";
+
+Heros heroThree = new Heros();
+heroThree.Name = "Bruce/David Banner";
+heroThree.HeroName = "Hulk";
+
+List<Heros> list = new List<Heros>();
+list.Add(heroOne);
+list.Add(heroTwo);
+list.Add(heroThree);
+
+foreach(Heros hero in list)
+{
+    Console.WriteLine(hero);
+}
